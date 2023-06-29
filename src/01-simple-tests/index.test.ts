90deb8754c8647f7d1d647e60438b67d31a09db4
Istiniel <eclipsio@mail.ru>
2023-06-29 21:50:20 +0300

    feat: simple and table tests done

--- src/01-simple-tests/index.test.ts
+++ src/01-simple-tests/index.test.ts
@@ -1,32 +1,39 @@
 // Uncomment the code below and write your tests
-// import { simpleCalculator, Action } from './index';
+import { simpleCalculator } from './index';
 
 describe('simpleCalculator tests', () => {
   test('should add two numbers', () => {
-    // Write your test here
+    const addNums = { a: 2, b: 3, action: '+' };
+    expect(simpleCalculator(addNums)).toBe(5);
   });
 
   test('should subtract two numbers', () => {
-    // Write your test here
+    const subtractNums = { a: 2, b: 3, action: '-' };
+    expect(simpleCalculator(subtractNums)).toBe(-1);
   });
 
   test('should multiply two numbers', () => {
-    // Write your test here
+    const multiplyNums = { a: 2, b: 3, action: '*' };
+    expect(simpleCalculator(multiplyNums)).toBe(6);
   });
 
   test('should divide two numbers', () => {
-    // Write your test here
+    const divideNums = { a: 6, b: 6, action: '/' };
+    expect(simpleCalculator(divideNums)).toBe(1);
   });
 
   test('should exponentiate two numbers', () => {
-    // Write your test here
+    const divideNums = { a: 2, b: 3, action: '^' };
+    expect(simpleCalculator(divideNums)).toBe(8);
   });
 
   test('should return null for invalid action', () => {
-    // Write your test here
+    const divideNums = { a: 2, b: 3, action: 'invalid action' };
+    expect(simpleCalculator(divideNums)).toBeNull();
   });
 
   test('should return null for invalid arguments', () => {
-    // Write your test here
+    const divideNums = { a: 2, b: 'invalid', action: '^' };
+    expect(simpleCalculator(divideNums)).toBeNull();
   });
 });

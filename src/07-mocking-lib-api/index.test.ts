c8801451d81e0e8c06b9f33ea13cdb10f742d43b
Istiniel <eclipsio@mail.ru>
2023-06-30 21:49:55 +0300

    mocking-lib-api part done

--- src/07-mocking-lib-api/index.test.ts
+++ src/07-mocking-lib-api/index.test.ts
@@ -1,17 +1,56 @@
 // Uncomment the code below and write your tests
-/* import axios from 'axios';
-import { throttledGetDataFromApi } from './index'; */
+import axios from 'axios';
+import { throttledGetDataFromApi } from './index';
 
 describe('throttledGetDataFromApi', () => {
   test('should create instance with provided base url', async () => {
-    // Write your test here
+    const createSpy = jest.spyOn(axios, 'create');
+    jest
+      .spyOn(axios.Axios.prototype, 'get')
+      .mockImplementationOnce(() => Promise.resolve({ data: 'test' }));
+
+    await throttledGetDataFromApi('baseUrl');
+    jest.runOnlyPendingTimers();
+
+    expect(createSpy).toBeCalledWith(
+      expect.objectContaining({
+        baseURL: 'https://jsonplaceholder.typicode.com',
+      }),
+    );
   });
 
   test('should perform request to correct provided url', async () => {
-    // Write your test here
+    const getSpy = jest
+      .spyOn(axios.Axios.prototype, 'get')
+      .mockImplementationOnce(() => Promise.resolve({ data: 'test2' }));
+
+    await throttledGetDataFromApi('/todos/1');
+    jest.runOnlyPendingTimers();
+
+    expect(getSpy).toBeCalledWith('/todos/1');
+    expect(getSpy).toHaveReturnedWith(Promise.resolve({ data: 'test2' }));
   });
 
   test('should return response data', async () => {
-    // Write your test here
+    const articles = [
+      { id: 1, title: 'article1' },
+      { id: 2, title: 'article2' },
+    ];
+
+    jest
+      .spyOn(axios.Axios.prototype, 'get')
+      .mockImplementationOnce(() => Promise.resolve({ data: articles }));
+
+    const data = await throttledGetDataFromApi('random-url');
+    jest.runOnlyPendingTimers();
+
+    expect(data[0].title).toBe('article1');
+  });
+
+  beforeAll(() => {
+    jest.useFakeTimers();
+  });
+  afterAll(() => {
+    jest.useRealTimers();
   });
 });
